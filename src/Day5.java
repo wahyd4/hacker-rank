98bcbc1aba05dfd4199416d760ec3a2c696b10bc
Junv Zhao <wahyd4@gmail.com>
2017-01-10 21:28:47 +0800

    add day 5

---
+++ src/Day5.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+/**
+ * https://www.hackerrank.com/challenges/30-loops
+ */
+public class Day5 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " x " + i + " = " + n * i);
+        }
+    }
+}
